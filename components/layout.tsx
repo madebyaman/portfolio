edd7ae17120d73b316d0e8098ee39d963d4147b8
Aman Thakur <builtbyaman@gmail.com>
2022-03-22 00:10:07 +0530

    added layout

---
+++ components/layout.tsx
@@ -0,0 +1,69 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from './layout.module.css';
+import Link from 'next/link';
+import { ReactNode } from 'react';
+
+const name = 'Aman Thakur';
+export const siteTitle = 'Aman Thakur Portfolio';
+
+export default function Layout({
+  children,
+  home,
+}: {
+  children: ReactNode;
+  home?: boolean;
+}) {
+  return (
+    <div>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <div className={styles.container}>
+        <header className={styles.header}>
+          {home ? (
+            <>
+              <Image
+                priority
+                src="/images/aman.png"
+                className={styles.headerImage}
+                height={144}
+                width={144}
+                alt={name}
+              />
+            </>
+          ) : (
+            <>
+              <Link href="/">
+                <a>
+                  <Image
+                    priority
+                    src="/images/aman.png"
+                    className={styles.headerImage}
+                    height={108}
+                    width={108}
+                    alt={name}
+                  />
+                </a>
+              </Link>
+            </>
+          )}
+        </header>
+        <main>{children}</main>
+        {!home && (
+          <div className={styles.backToHome}>
+            <Link href="/">
+              <a>← Back to home</a>
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import {
   skillsList,
   socialLinks,
 } from '../components/data';
+import Layout from '../components/layout';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
@@ -23,191 +24,171 @@ const Home: NextPage = () => {
   };
 
   return (
-    <div>
+    <Layout home>
       <Head>
         <title>Aman Thakur Portfolio</title>
       </Head>
 
-      <main>
-        <div className={styles.heroFlex}>
-          <div className={styles.heroImage}>
-            <Image
-              src="https://res.cloudinary.com/dksughwo7/image/upload/v1646323986/book-notes-app/profile/yd3ziylngywgbmbnlc48.png"
-              width={175}
-              height={175}
-              alt="Aman Thakur"
-            />
-          </div>
+      <section className={styles.hero}>
+        <div>
+          <h1>Hi, I am Aman</h1>
           <div>
-            <section className={styles.hero}>
-              <div>
-                <h1>Hi, I am Aman</h1>
-                <div>
-                  <p>
-                    A web developer living in India 🇮🇳, a dog lover 🐶, and a
-                    cricket enthusiast 🏏. I am currently looking for job as a
-                    frontend developer 💻.
-                  </p>
-                  <p>
-                    At the same time, I am working on cool projects, including
-                    Bummaries, an app to write your book notes.
-                  </p>
-                  <p>
-                    📖 Currently reading{' '}
-                    <Link
-                      href="https://www.amazon.com/Stress-Free-Productivity-Personalized-Efficient-Creative/dp/0593191331"
-                      passHref
-                    >
-                      <a target="_blank">
-                        Stress Free Productivity by Alice Boyes
-                      </a>
-                    </Link>
-                  </p>
-                  <div className={styles.social}>
-                    {socialLinks.map((link) => (
+            <p>
+              A web developer living in India 🇮🇳, a dog lover 🐶, and a cricket
+              enthusiast 🏏. I am currently looking for job as a frontend
+              developer 💻.
+            </p>
+            <p>
+              At the same time, I am working on cool projects, including
+              Bummaries, an app to write your book notes.
+            </p>
+            <p>
+              📖 Currently reading{' '}
+              <Link
+                href="https://www.amazon.com/Stress-Free-Productivity-Personalized-Efficient-Creative/dp/0593191331"
+                passHref
+              >
+                <a target="_blank">Stress Free Productivity by Alice Boyes</a>
+              </Link>
+            </p>
+            <div className={styles.social}>
+              {socialLinks.map((link) => (
+                <Link key={link.name} href={link.url} passHref>
+                  <a target="_blank">{link.icon}</a>
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className={styles.skills}>
+        <h2>Technical Skill Set</h2>
+        <div className={styles.skillsList}>
+          {skillsList.map((skill) => (
+            <div key={skill.name} className={styles.skill}>
+              <div className={styles.skillIcon}>{skill.icon}</div>
+              <p>{skill.name}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className={styles.projects}>
+        <div className={styles.heading}>
+          <h2>Projects</h2>
+        </div>
+        <div className={styles.projectList}>
+          {projectsList.filter(filterProjects).map((project) => {
+            const { highlighted, backgroundColor } = project;
+            return (
+              <div
+                key={project.name}
+                className={
+                  highlighted
+                    ? styles.project + ` ${styles.highlightedProject}`
+                    : styles.project
+                }
+                style={{
+                  backgroundColor: highlighted ? backgroundColor : 'white',
+                }}
+              >
+                <div className={styles.projectText}>
+                  <h3>{project.name}</h3>
+                  <p>{project.description}</p>
+                  <div className={styles.projectLinks}>
+                    {project.links.map((link) => (
                       <Link key={link.name} href={link.url} passHref>
-                        <a target="_blank">{link.icon}</a>
+                        <a target={link.externalLink ? '_blank' : '_self'}>
+                          {link.externalLink && (
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              style={{
+                                width: '18px',
+                                height: '18px',
+                              }}
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                              strokeWidth={2}
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                              />
+                            </svg>
+                          )}
+                          {link.name}
+                        </a>
                       </Link>
                     ))}
                   </div>
                 </div>
-              </div>
-            </section>
-            <section className={styles.skills}>
-              <h2>Technical Skill Set</h2>
-              <div className={styles.skillsList}>
-                {skillsList.map((skill) => (
-                  <div key={skill.name} className={styles.skill}>
-                    <div className={styles.skillIcon}>{skill.icon}</div>
-                    <p>{skill.name}</p>
-                  </div>
-                ))}
-              </div>
-            </section>
-            <section className={styles.projects}>
-              <div className={styles.heading}>
-                <h2>Projects</h2>
-              </div>
-              <div className={styles.projectList}>
-                {projectsList.filter(filterProjects).map((project) => {
-                  const { highlighted, backgroundColor } = project;
-                  return (
-                    <div
-                      key={project.name}
-                      className={
-                        highlighted
-                          ? styles.project + ` ${styles.highlightedProject}`
-                          : styles.project
-                      }
-                      style={{
-                        backgroundColor: highlighted
-                          ? backgroundColor
-                          : 'white',
-                      }}
-                    >
-                      <div className={styles.projectText}>
-                        <h3>{project.name}</h3>
-                        <p>{project.description}</p>
-                        <div className={styles.projectLinks}>
-                          {project.links.map((link) => (
-                            <Link key={link.name} href={link.url} passHref>
-                              <a
-                                target={link.externalLink ? '_blank' : '_self'}
-                              >
-                                {link.externalLink && (
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    style={{
-                                      width: '18px',
-                                      height: '18px',
-                                    }}
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                    strokeWidth={2}
-                                  >
-                                    <path
-                                      strokeLinecap="round"
-                                      strokeLinejoin="round"
-                                      d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-                                    />
-                                  </svg>
-                                )}
-                                {link.name}
-                              </a>
-                            </Link>
-                          ))}
-                        </div>
-                      </div>
-                      <div className={styles.projectImage}>
-                        {project.screenshot && (
-                          <Image
-                            src={project.screenshot}
-                            width={700}
-                            height={736}
-                            alt={project.name}
-                          />
-                        )}
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
-              {!showAllProjects && (
-                <a
-                  onClick={() => setShowAllProjects((prev) => !prev)}
-                  className={styles.allProjectsLink}
-                >
-                  Show more projects
-                </a>
-              )}
-            </section>
-            <section className={styles.resume}>
-              <h2>Resume</h2>
-              <Link href="/resume.pdf" passHref>
-                <a target="_blank">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    style={{
-                      width: '19px',
-                      height: '19px',
-                    }}
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
+                <div className={styles.projectImage}>
+                  {project.screenshot && (
+                    <Image
+                      src={project.screenshot}
+                      width={700}
+                      height={736}
+                      alt={project.name}
                     />
-                  </svg>
-                  Download resume
-                </a>
-              </Link>
-            </section>
-            <section className={styles.contactForm}>
-              <h2>Contact</h2>
-              <form action="/success" method="post" data-netlify="true">
-                <label>
-                  Name
-                  <input type="text" required name="Name" />
-                </label>
-                <label>
-                  Email
-                  <input type="email" required name="Name" />
-                </label>
-                <label>
-                  Message: <textarea />
-                </label>
-                <input type="submit" />
-              </form>
-            </section>
-          </div>
+                  )}
+                </div>
+              </div>
+            );
+          })}
         </div>
-      </main>
-    </div>
+        {!showAllProjects && (
+          <a
+            onClick={() => setShowAllProjects((prev) => !prev)}
+            className={styles.allProjectsLink}
+          >
+            Show more projects
+          </a>
+        )}
+      </section>
+      <section className={styles.resume}>
+        <h2>Resume</h2>
+        <Link href="/resume.pdf" passHref>
+          <a target="_blank">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              style={{
+                width: '19px',
+                height: '19px',
+              }}
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
+              />
+            </svg>
+            Download resume
+          </a>
+        </Link>
+      </section>
+      <section className={styles.contactForm}>
+        <h2>Contact</h2>
+        <form action="/success" method="post" data-netlify="true">
+          <label>
+            Name
+            <input type="text" required name="Name" />
+          </label>
+          <label>
+            Email
+            <input type="email" required name="Name" />
+          </label>
+          <label>
+            Message: <textarea />
+          </label>
+          <input type="submit" />
+        </form>
+      </section>
+    </Layout>
   );
 };
 
